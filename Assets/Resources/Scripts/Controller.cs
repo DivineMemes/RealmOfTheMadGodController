e5c79dabd79015990358026e806668188f188efe
Wyatt Gallagher <s179053@students.aie.edu.au>
2018-12-10 09:32:53 -0800

    first commit

---
+++ Assets/Resources/Scripts/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float zAxis;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.down);
+        }
+        else if(Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.up);
+        }
+        
+    }
+}
